802d94b48431f026e03cd9e1f11ba7a323c511ce
jaal9709 <jaal9709@gmail.com>
2024-05-17 14:59:01 -0500

    Cambios en las generaciones de pokemon agregado mapas y descripción

--- src/app/components/GenerationCard.tsx
+++ src/app/components/GenerationCard.tsx
@@ -11,7 +11,7 @@ const GenenerationCard = (props: any) => {
     const [generation, setGeneration] = useState([]);
     const [isLoading, setLoading] = useState(true);
 
-    
+
 
     let url = props.url;
     let data = props.data;
@@ -42,27 +42,18 @@ const GenenerationCard = (props: any) => {
 
     // console.log(generation)
     // console.log(generation.main_region.name);
-    console.log(data);
+    console.log(generation);
 
     return (
         <Link href={`generation/${generation.id}`} data={generation} id={generation.id}>
             <div
                 className="block max-w-sm  md:max-w-xs m-4  lg:max-w-md md:h-max  rounded-lg bg-yellow-100 shadow-secondary-1 opacity-90  mt-6 p-4">
                 <div className=" text-surface flex justify-center">
-
-                    {
-                        generation.main_region.name === 'unova'
-
-                            ? <h5 className="mt-2 text-3xl font-bold  text-slate-950 capitalize font-heading 
-                            ">
-                                {generation.main_region.name} / Teselia
-                            </h5>
-
-                            : <h5 className="mt-2 text-3xl font-bold  text-slate-950 capitalize font-heading">
-                                {generation.main_region.name}
-                            </h5>
-                    }
+                    <h5 className="mt-2 text-3xl font-bold  text-slate-950 capitalize font-heading text-center">
+                        {generation.names[5].name.split('Generación ')} Generación
+                    </h5>
                 </div>
+
 
                 <div className='flex justify-center '>
                     <img
--- src/app/components/GenerationDetails.tsx
+++ src/app/components/GenerationDetails.tsx
@@ -3,10 +3,10 @@ import { useEffect, useState } from 'react';
 import { useParams, useRouter } from 'next/navigation';
 import { generacionesDB } from '@/utils/generacionesDB';
 
-const GenenerationDetails = (props:any) => {
+const GenenerationDetails = (props: any) => {
 
     let id = props.id;
-    
+
     let generaciones = generacionesDB;
 
     const [generation, setGeneration] = useState([]);
@@ -16,22 +16,22 @@ const GenenerationDetails = (props:any) => {
 
 
     useEffect(() => {
-         fetch(url, {
-             method: 'GET',
-             headers: {
-                 "Content-Type": "application/json",
-             },
-         })
-             .then((res) => res.json())
-             .then((data) => {
-                 setGeneration(data)
-                 setLoading(false)
-             })
-     }, [id, isLoading])
+        fetch(url, {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                setGeneration(data)
+                setLoading(false)
+            })
+    }, [id, isLoading])
 
-    console.log(generation);
+    // console.log(generation.names[5].name);
 
-    console.log(generacionesDB[id-1]);
+    // console.log(generacionesDB[id - 1]);
 
     if (isLoading) {
         return <div className='flex items-center justify-center h-screen bg-gray-100'>
@@ -41,36 +41,74 @@ const GenenerationDetails = (props:any) => {
     }
 
     return (
-        <div className="p-6 text-surface dark:text-white">
-            {
-                generation.name === 'unova'
 
-                ?    <h5 className="mb-2 text-xl font-medium leading-tight text-slate-900">{generation.main_region.name} / Teselia</h5>
-
-                :    <h5 className="mb-2 text-xl font-medium leading-tight text-slate-900 capitalize">{generation.main_region.name}</h5>
-            }
+        <div className="p-6 m-6 relative text-surface flex flex-col justify-center bg-yellow-100 opacity-95 w-4/5  max-w-[60rem] mx-auto rounded-lg">
            
-            <p className="mb-4 text-base text-fuchsia-950">
-                {/* {data.main_region.name} */}
-            </p>
-            <h3 className='text-fuchsia-950 mt-2 mb-3'>Juegos</h3>
-            <div className='mt-2 grid grid-cols-2 gap-1  justify-evenly md:min-h-80'>
+            <h5 className=" text-3xl font-bold text-slate-700 capitalize font-heading text-center ">{generation.names[5].name.split('Generación ')} Generación </h5>
+            {
+                generation.main_region.name === 'unova'
+                        
+                    ? <p className='text-center mb-2 text-xl font-medium text-slate-600 capitalize '>({generation.main_region.name} / Teselia) </p>
+
+                    : <p className='text-center mb-2 text-xl font-medium text-slate-600 capitalize'>({generation.main_region.name})</p>
+            }
+
+            <picture className='flex justify-center w-full'>
+                <img src={generaciones[id-1].image} className='rounded-lg ' alt="img_generation" />
+            </picture>
+
+            <h3 className='mb-4 mt-5 text-xl font-bold text-slate-700 capitalize font-heading text-center'>Juegos o Versiones</h3>
+            <div className='mt-2 grid grid-cols-2  sm:grid-cols-3 gap-1  justify-between '>
 
                 {generation.version_groups.map((version) => (
-                    <p className="mb-4 text-base text-fuchsia-950 w-26">
+                    <p className="mb-4 font-bold text-xl text-amber-700 capitalize" key={generation.version_groups.indexOf(version)}>
                         {version.name}
                     </p>
                 ))}
+
             </div>
+
             <div>
+                <p className='text-justify text-lg'>
+                    {generaciones[id-1].description}
+                </p>
+            </div>
 
-               
+           
+            {
+                generation.main_region.name === 'unova'
+                        
+                    ? <h5 className='mb-4 mt-5 text-xl font-bold text-slate-700 capitalize font-heading text-center '>Mapa de {generation.main_region.name} / Teselia </h5>
 
+                    : <h5 className='mb-4 mt-5 text-xl font-bold text-slate-700  font-heading text-center capitalize'>Mapa de {generation.main_region.name} </h5>
+            }
+           
 
+            <picture className='flex justify-center'>
+                <img src={generaciones[id-1].map_img} alt="" />
+            </picture>
+
+            <div>
+                <p className='text-justify text-lg mt-4'>
+                    {generaciones[id-1].description_2}
+                </p>
             </div>
 
         </div>
+
+
     )
 }
+
+{/* urls imagenes */ }
+
+{/* https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/333.png */ }
+
+{/* https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png */ }
+
+
+{/* pokedex
+
+            https://pokeapi.co/api/v2/pokedex/1/ */}
 
 export default GenenerationDetails
