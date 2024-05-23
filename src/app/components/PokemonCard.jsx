60b399c017f64c415fceff12efd37280c77af28f
jaal9709 <jaal9709@gmail.com>
2024-05-23 16:34:02 -0500

    se han conseguido algunos datos de los pokemons pero falta ajustarlos

--- src/app/components/PokemonCard.jsx
+++ src/app/components/PokemonCard.jsx
@@ -10,12 +10,42 @@ const PokemonCard = (props) => {
 
     const params = useParams()
     let data = params.data;
-    let poke_in = generaciones[data-1].pokemons[0]
-    let poke_end = generaciones[data-1].pokemons[1]
-   
+    let poke_in = generaciones[data - 1].pokemons[0] - 1
+    let poke_end = generaciones[data - 1].pokemons[1]
+
+
+    // guardar los datos de los pokemons
+
+    //datos generales
+    let pokemons_data = [];
+
+    //numero en la pokedex
+    let poke_nums = [];
+
+    //nombre del Pokémon
+    let poke_names = [];
+
+    //imagen del Pokémon
+    let poke_img_p = [];
+
+    const [pokeTotal, setPokeTotal] = useState([{
+        num: 0,
+        name: '',
+        img: '',
+    }]);
+    // let pokemons_total = {
+    //      num: 0,
+    //      name: '',
+    //      img: ''
+    // }
+
+    // pokemons_total.num = poke_nums;
+    // pokemons_total.name = poke_names;
+    // pokemons_total.img = poke_img_p;
 
 
     const [pokemons, setPokemons] = useState([]);
+    const [poke, setPoke] = useState([]);
     const [isLoading, setLoading] = useState(true);
 
     let url = `${process.env.NEXT_PUBLIC_API_URL}/pokedex/1`;
@@ -41,14 +71,32 @@ const PokemonCard = (props) => {
         </div>
     }
 
-    console.log(poke_in);
-    console.log(poke_end);
-    // console.log(generaciones[data-1].pokemons)
-    console.log(pokemons.pokemon_entries[poke_in -1]);
+    // console.log(poke_in);
+    // console.log(poke_end)
 
-  return (
-    <div>PokemonCard</div>
-  )
+    // console.log(generaciones[data-1].pokemons)
+    pokemons_data = pokemons.pokemon_entries.slice(poke_in, poke_end)
+    // console.log(pokemons.pokemon_entries.slice(poke_in,poke_end));
+    // console.log(pokemons_data)
+
+    for (const p in pokemons_data) {
+        setPokeTotal.name(pokemons_data[p].pokemon_species.name)
+        // poke_nums.push(pokemons_data[p].entry_number)
+        pokeTotal.num =pokemons_data[p].entry_number
+        // poke_img_p.push(`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemons_data[p].entry_number}.png`)
+        pokeTotal.img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemons_data[p].entry_number}.png`
+    }
+    // console.log(poke_names);
+    console.log(pokeTotal);
+    return (
+        <div>
+            <div className=' '>
+                {poke_nums.map((num) => (
+                    <span key={num}>{num}</span>
+                ))}
+            </div>
+        </div>
+    )
 }
 
 export default PokemonCard
